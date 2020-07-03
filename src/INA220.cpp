841383e3c075f1649a19f1710c8b1c8b1c89bfef
Nathan Cheek <nathan@nathancheek.com>
2020-07-02 22:52:54 -0500

    Add initial library files

---
+++ src/INA220.cpp
@@ -0,0 +1,294 @@
+#include "INA220.h"
+#include <Wire.h>
+
+INA220::INA220() {
+}
+
+uint8_t INA220::begin(uint8_t maxBusAmps, uint32_t microOhmR, const ina_Adc_Mode busAdcMode, const ina_Adc_Mode shuntAdcMode, const ina_Mode deviceMode, uint8_t* deviceAddresses, uint8_t numDevices) {
+  /*! @brief     Initializes the contents of the class
+      @details   Sets INA Configuration registers for the devices at the specified addresses.
+      @param[in] maxBusAmps Integer value holding the maximum expected bus amperage, this value is used to
+                 compute each device's internal power register
+      @param[in] microOhmR Shunt resistance in micro-ohms, this value is used to compute each device's internal
+                 power register
+      @param[in] busAdcMode 4-bit mode selector for the Bus ADC (see INA_ADC_MODE_* for possible modes)
+      @param[in] shuntAdcMode 4-bit mode selector for the Shunt ADC (see INA_ADC_MODE_* for possible modes)
+      @param[in] deviceMode 3-bit device mode selector (see ina_Mode for possible modes)
+      @param[in] deviceAddresses pointer to array of device addresses
+      @param[in] numDevices number of devices enumerated in deviceAddresses array
+      @return    Number of INA220 devices found on the I2C bus */
+
+  this->deviceAddresses      = deviceAddresses;
+  this->numDevices           = numDevices;
+  this->configRegister       = INA220_CONFIG_DEFAULT; // Initialize to the INA220 default value
+  this->current_LSB          = (uint64_t)maxBusAmps * 1000000000 / 32767; // Get the best possible LSB in nA
+  this->power_LSB            = (uint32_t)20 * this->current_LSB;          // Fixed multiplier per device
+  this->calibrationRegister  = (uint64_t)409600000 / ((uint64_t)this->current_LSB * (uint64_t)microOhmR / (uint64_t)100000); // Compute calibration register
+
+  /* Determine optimal programmable gain so that there is no chance of an overflow yet with maximum accuracy */
+  uint8_t programmableGain;                                              // work variable for the programmable gain
+  uint16_t maxShuntmV = maxBusAmps * microOhmR / 1000;                   // Compute maximum shunt millivolts
+  if      (maxShuntmV <= 40)  programmableGain = 0;                      // gain x1 for +- 40mV
+  else if (maxShuntmV <= 80)  programmableGain = 1;                      // gain x2 for +- 80mV
+  else if (maxShuntmV <= 160) programmableGain = 2;                      // gain x4 for +- 160mV
+  else                        programmableGain = 3;                      // default gain x8 for +- 320mV
+  this->configRegister  = INA220_CONFIG_DEFAULT & INA220_CONFIG_PG_MASK; // Zero out the programmable gain
+  this->configRegister |= programmableGain << INA220_PG_FIRST_BIT;       // Overwrite the new values
+  bitSet(this->configRegister, INA220_BRNG_BIT);                         // set to 1 for 0-32 volts
+
+  /* Place ADC mode bits in configuration register */
+  this->configRegister &= ~INA220_CONFIG_ADCMODE_MASK;                   // Zero out the ADC mode bits
+  this->configRegister |= (busAdcMode & 0xF)   << INA220_BADC_FIRST_BIT; // Mask off unused bits then shift in the bus ADC mode
+  this->configRegister |= (shuntAdcMode & 0xF) << INA220_SADC_FIRST_BIT; // Mask off unused bits then shift in the shunt ADC mode
+
+  /* Place device mode bits in configuration register */
+  this->configRegister &= ~INA_CONFIG_MODE_MASK;                         // Zero out the device mode bits
+  this->configRegister |= INA_CONFIG_MODE_MASK & deviceMode;             // Mask off unused bits then shift in the mode settings
+
+  Wire.begin();
+  uint8_t availableDevices = resetAll(); // Check if communication is working, then write calibration and configuration registers
+  return availableDevices;
+}
+
+void INA220::setI2CSpeed(const uint32_t i2cSpeed) {
+  /*! @brief     Set a new I2C speed
+      @details   I2C allows various bus speeds, see the enumerated type I2C_MODES for the standard speeds. The valid
+                 speeds are  100KHz, 400KHz, 1MHz and 3.4MHz. Default to 100KHz when not specified. No range checking
+                 is done.
+      @param[in] i2cSpeed [optional] changes the I2C speed to the rate specified in Hertz */
+
+  Wire.setClock(i2cSpeed);
+}
+
+void INA220::setMode(const uint8_t mode, const uint8_t deviceNumber) {
+  /*! @brief     Sets the operating mode from the list given in enumerated type "ina_Mode" for a device
+      @param[in] mode Mode (see "ina_Mode" enumerated type for list of valid values
+      @param[in] deviceNumber Number of device to configure */
+
+  int16_t configRegister;
+  configRegister = readWord(INA_CONFIGURATION_REGISTER, getDeviceAddress(deviceNumber)); // Get the current register from device
+  configRegister &= ~INA_CONFIG_MODE_MASK;                                               // zero out the mode bits
+  configRegister |= INA_CONFIG_MODE_MASK & mode;                                         // Mask off unused bits then shift in the mode settings
+  writeWord(INA_CONFIGURATION_REGISTER,configRegister, getDeviceAddress(deviceNumber));  // Save new value
+}
+
+void INA220::setModeAll(const uint8_t mode) {
+  /*! @brief     Sets the operating mode from the list given in enumerated type "ina_Mode" for all devices
+      @param[in] mode Mode (see "ina_Mode" enumerated type for list of valid values */
+
+  for (int i = 0; i < this->numDevices; i++) {
+    setMode(mode, i);
+  }
+}
+
+uint16_t INA220::getBusMilliVolts(const uint8_t deviceNumber) {
+  /*! @brief     Returns the bus voltage in millivolts
+      @details   The converted millivolt value is returned
+      @param[in] deviceNumber to return the device bus millivolts for
+      @return    uint16_t unsigned integer for the bus millivoltage */
+
+  uint16_t busVoltage = getBusRaw(deviceNumber);                             // Get raw voltage from device
+  busVoltage          = (uint32_t)busVoltage * INA220_BUS_VOLTAGE_LSB / 100; // conversion to get milliVolts
+  return busVoltage;
+}
+
+uint16_t INA220::getBusRaw(const uint8_t deviceNumber) {
+  /*! @brief     Returns the raw unconverted bus voltage reading from the device
+      @details   The raw measured value is returned
+      @param[in] deviceNumber to return the raw device bus voltage reading
+      @return    Raw bus measurement */
+
+  uint16_t raw = readWord(INA_BUS_VOLTAGE_REGISTER, getDeviceAddress(deviceNumber)); // Get the raw value from register
+  raw = raw >> 3; // INA220 - the 3 LSB unused, so shift right
+  return(raw);
+}
+
+int32_t INA220::getShuntMicroVolts(const uint8_t deviceNumber) {
+  /*! @brief     Returns the shunt reading converted to microvolts
+      @details   The computed microvolts value is returned
+      @param[in] deviceNumber to return the value for
+      @return    int32_t signed integer for the shunt microvolts */
+
+  int32_t shuntVoltage = getShuntRaw(deviceNumber);
+  shuntVoltage = shuntVoltage * INA220_SHUNT_VOLTAGE_LSB / 10; // Convert to microvolts
+  return(shuntVoltage);
+}
+
+int16_t INA220::getShuntRaw(const uint8_t deviceNumber) {
+  /*! @brief     Returns the raw shunt reading
+      @details   The raw reading is returned
+      @param[in] deviceNumber to return the value for
+      @return    Raw shunt reading */
+
+  int16_t raw = readWord(INA220_SHUNT_VOLTAGE_REGISTER, getDeviceAddress(deviceNumber)); // Get the raw value from register
+  return(raw);
+}
+
+int32_t INA220::getBusMicroAmps(const uint8_t deviceNumber) {
+  /*! @brief     Returns the computed microamps measured on the bus for the specified device
+      @details   The computed reading is returned
+      @param[in] deviceNumber to return the value for
+      @return    int32_t signed integer for computed microamps on the bus */
+
+  int32_t microAmps = (int64_t)readWord(INA220_CURRENT_REGISTER, getDeviceAddress(deviceNumber)) * this->current_LSB / 1000;
+  return(microAmps);
+}
+
+int32_t INA220::getBusMicroWatts(const uint8_t deviceNumber) {
+  /*! @brief     Returns the computed microwatts measured on the bus for the specified device
+      @details   The computed reading is returned
+      @param[in] deviceNumber to return the value for
+      @return    int32_t signed integer for computed microwatts on the bus */
+
+  int32_t microWatts = (int64_t)readWord(INA_POWER_REGISTER, getDeviceAddress(deviceNumber)) * this->power_LSB / 1000;
+  return(microWatts);
+}
+
+void INA220::triggerConversion(const uint8_t deviceNumber) {
+  /*! @brief     Starts a new conversion if the device is in triggered mode
+      @details   Writes back the configuration register, triggering shunt, bus, or both conversions based on the device mode
+      @param[in] deviceNumber to return the value for */
+
+  int16_t configRegister = readWord(INA_CONFIGURATION_REGISTER, getDeviceAddress(deviceNumber)); // Get the current register
+  writeWord(INA_CONFIGURATION_REGISTER, configRegister, getDeviceAddress(deviceNumber)); // Write back to trigger next conversion
+}
+
+void INA220::triggerConversionAll() {
+  /*! @brief     Starts a new conversion for all devices if they are in triggered mode
+      @details   Writes back the configuration register, triggering shunt, bus, or both conversions based on the device mode */
+
+  for (int i = 0; i < this->numDevices; i++) {
+    triggerConversion(i);
+  }
+}
+
+uint16_t INA220::getMode(const uint8_t deviceNumber) {
+  uint16_t configRegister = readWord(INA_CONFIGURATION_REGISTER, getDeviceAddress(deviceNumber));
+  return configRegister & INA_CONFIG_MODE_MASK;
+}
+
+uint8_t INA220::getDeviceAddress(const uint8_t deviceNumber) {
+  /*! @brief     Returns a I2C address of the device specified in the input parameter
+      @details   Return the I2C address of the specified device, if the number is out of range 0 is returned
+      @param[in] deviceNumber to return the device name of
+      @return    I2C address of the device. Returns 0 if value is out-of-range */
+
+  if (deviceNumber > this->numDevices) return 0;
+  return(this->deviceAddresses[deviceNumber]);
+}
+
+uint8_t INA220::reset(const uint8_t deviceNumber) {
+  /*! @brief     Performs a software reset for the specified device
+      @details   Device is reset to default settings along with any parameters given when begin() was last called
+      @param[in] deviceNumber to reset
+      @return    uint8_t number of devices that identified as INA220 (0 or 1) */
+
+  uint8_t availableDevices = 0;
+  Wire.beginTransmission(getDeviceAddress(deviceNumber));
+  uint8_t good = Wire.endTransmission();
+  if (good == 0) { // If no I2C error
+    writeWord(INA_CONFIGURATION_REGISTER, INA_RESET_DEVICE, getDeviceAddress(deviceNumber));          // Forces INAs to reset
+    uint16_t tempRegister = readWord(INA_CONFIGURATION_REGISTER, getDeviceAddress(deviceNumber));     // Read the newly reset register
+    if (tempRegister == INA220_CONFIG_DEFAULT) {
+      availableDevices = 1; // Responded correctly with INA220 default configuration register value
+    }
+  }
+  initDevice(deviceNumber);
+  return availableDevices;
+}
+
+uint8_t INA220::resetAll() {
+  /*! @brief     Performs a software reset for all devices
+      @details   Devices are reset to default settings along with any parameters given when begin() was last called
+      @return    uint8_t number of devices that identified as INA220 */
+
+  uint8_t availableDevices = 0;
+  for (int i = 0; i < this->numDevices; i++) {
+    availableDevices += reset(i);
+  }
+  return availableDevices;
+}
+
+bool INA220::conversionFinished(const uint8_t deviceNumber) {
+  /*! @brief     Returns whether or not the conversion has completed
+      @details   The device's conversion ready bit is read and returned. "true" denotes finished conversion.
+      @param[in] deviceNumber to check */
+
+  uint16_t cvBits = readWord(INA_BUS_VOLTAGE_REGISTER, getDeviceAddress(deviceNumber)) & 2; // Bit 2 set denotes ready
+  readWord(INA_POWER_REGISTER, getDeviceAddress(deviceNumber));                             // Resets the "ready" bit
+  if (cvBits != 0) return(true); else return(false);
+}
+
+bool INA220::waitForConversion(const uint16_t timeout, const uint8_t deviceNumber) {
+  /*! @brief     Waits until the conversion for the specified device is finished, or timeout is exceeded
+      @param[in] timeout Time to wait in ms before timing out
+      @param[in] deviceNumber to reset
+      @return    true if successful, false if timeout was exceeded */
+
+  long startMs = millis();
+  uint16_t cvBits = 0;
+  while (cvBits == 0) {                                                              // Loop until the value is set or timeout is exceeded
+    cvBits = readWord(INA_BUS_VOLTAGE_REGISTER, getDeviceAddress(deviceNumber)) & 2; // Bit 2 set denotes ready
+    readWord(INA_POWER_REGISTER, getDeviceAddress(deviceNumber));                    // Resets the "ready" bit
+    if (millis() - startMs < timeout) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+uint8_t INA220::waitForConversionAll(const uint16_t timeout) {
+  /*! @brief     Waits until the conversion for all devices is finished, or timeout is exceeded
+      @param[in] timeout Time to wait for each device in ms before timing out
+      @return    number of completed conversions */
+
+  uint8_t successCount = 0;
+  for (int i = 0; i < this->numDevices; i++) {
+    successCount += waitForConversion(timeout, i);
+  }
+  return successCount;
+}
+
+void INA220::initDevice(const uint8_t deviceNumber) {
+  /*! @brief     Initializes the given devices using the settings configured with begin()
+      @param[in] deviceNumber Device number to explicitly initialize. */
+
+  writeWord(INA_CALIBRATION_REGISTER, this->calibrationRegister, getDeviceAddress(deviceNumber)); // Write calibration value to device register
+  writeWord(INA_CONFIGURATION_REGISTER, this->configRegister, getDeviceAddress(deviceNumber));    // Write new value to config register
+}
+
+int16_t INA220::readWord(const uint8_t addr, const uint8_t deviceAddress) {
+  /*! @brief     Read one word (2 bytes) from the specified I2C address
+      @details   Standard I2C protocol is used, but a delay of I2C_DELAY microseconds has been added to let the INAxxx
+                 devices have sufficient time to get the return data ready.
+      @param[in] addr I2C address to read from
+      @param[in] deviceAddress Address on the I2C device to read from
+      @return    integer value read from the I2C device */
+
+  Wire.beginTransmission(deviceAddress);       // Address the I2C device
+  Wire.write(addr);                            // Send register address to read
+  Wire.endTransmission();                      // Close transmission
+  delayMicroseconds(I2C_DELAY);                // Delay required for sync
+  Wire.requestFrom(deviceAddress, (uint8_t)2); // Request 2 consecutive bytes
+  int16_t returnData  = Wire.read();           // Read the msb
+  returnData  = returnData << 8;               // Shift the data over 8 bits
+  returnData |= Wire.read();                   // Read the lsb
+  return returnData;
+}
+
+void INA220::writeWord(const uint8_t addr, const uint16_t data, const uint8_t deviceAddress) {
+  /*! @brief     Write 2 bytes to the specified I2C address
+      @details   Standard I2C protocol is used, but a delay of I2C_DELAY microseconds has been added to let the INAxxx
+                 devices have sufficient time to process the data
+      @param[in] addr I2C address to write to
+      @param[in] data 2 Bytes to write to the device
+      @param[in] deviceAddress Address on the I2C device to write to */
+
+  Wire.beginTransmission(deviceAddress); // Address the I2C device
+  Wire.write(addr);                      // Send register address to write
+  Wire.write((uint8_t)(data >> 8));      // Write the first (MSB) byte
+  Wire.write((uint8_t)data);             // And then the second
+  Wire.endTransmission();                // Close transmission and actually send data
+  delayMicroseconds(I2C_DELAY);          // Delay required for sync
+}
---
+++ src/INA220.h
@@ -0,0 +1,126 @@
+/***************************************************************************************************************//*!
+* Contact - Nathan Cheek - ncheek@gatech.edu
+*
+* This project is based largely off of https://github.com/SV-Zanshin/INA
+* Both this project and SV-Zanshin/INA are licensed under GNU General Public License v3.0
+
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*******************************************************************************************************************/
+ 
+#include "Arduino.h"
+
+#ifndef INA220__Class_h
+  /*! Guard code definition to prevent multiple includes */
+  #define INA220__Class_h
+
+  /*************************************************************************************************************//*!
+  * Enumerated list detailing the operating modes of the INA220
+  *****************************************************************************************************************/
+  enum ina_Mode
+  {
+    INA_MODE_SHUTDOWN,         ///< Device powered down
+    INA_MODE_TRIGGERED_SHUNT,  ///< Triggered shunt, no bus
+    INA_MODE_TRIGGERED_BUS,    ///< Triggered bus, no shunt
+    INA_MODE_TRIGGERED_BOTH,   ///< Triggered bus and shunt
+    INA_MODE_POWER_DOWN,       ///< Shutdown or power-down
+    INA_MODE_CONTINUOUS_SHUNT, ///< Continuous shunt, no bus
+    INA_MODE_CONTINUOUS_BUS,   ///< Continuous bus, no shunt
+    INA_MODE_CONTINUOUS_BOTH   ///< Both continuous, default value
+  }; // of enumerated type
+
+  /*************************************************************************************************************//*!
+  * Enumerated list detailing the ADC modes of the INA220
+  *****************************************************************************************************************/
+  enum ina_Adc_Mode
+  {
+    INA_ADC_MODE_9BIT   =  0x0,
+    INA_ADC_MODE_10BIT  =  0x1,
+    INA_ADC_MODE_11BIT  =  0x2,
+    INA_ADC_MODE_12BIT  =  0x3,
+    INA_ADC_MODE_2AVG   =  0x9,
+    INA_ADC_MODE_4AVG   = 0x10,
+    INA_ADC_MODE_8AVG   = 0x11,
+    INA_ADC_MODE_16AVG  = 0x12,
+    INA_ADC_MODE_32AVG  = 0x13,
+    INA_ADC_MODE_64AVG  = 0x14,
+    INA_ADC_MODE_128AVG = 0x15
+  }; // of enumerated type
+
+  /*****************************************************************************************************************
+  ** Declare constants used in the class                                                                          **
+  *****************************************************************************************************************/
+  #ifndef I2C_MODES   // I2C related constants
+    #define I2C_MODES // Guard code to prevent multiple definitions
+    const uint32_t INA_I2C_STANDARD_MODE        =  100000; ///< Default normal I2C 100KHz speed
+    const uint32_t INA_I2C_FAST_MODE            =  400000; ///< Fast mode
+    const uint32_t INA_I2C_FAST_MODE_PLUS       = 1000000; ///< Really fast mode
+    const uint32_t INA_I2C_HIGH_SPEED_MODE      = 3400000; ///< Turbo mode
+  #endif
+  const uint8_t  INA_CONFIGURATION_REGISTER     =       0; ///< Configuration Register address
+  const uint8_t  INA_BUS_VOLTAGE_REGISTER       =       2; ///< Bus Voltage Register address
+  const uint8_t  INA_POWER_REGISTER             =       3; ///< Power Register address
+  const uint8_t  INA_CALIBRATION_REGISTER       =       5; ///< Calibration Register address
+  const uint16_t INA_RESET_DEVICE               =  0x8000; ///< Write to configuration to reset device
+  const uint16_t INA_CONVERSION_READY_MASK      =  0x0080; ///< Bit 4
+  const uint16_t INA_CONFIG_MODE_MASK           =  0x0007; ///< Bits 0-3
+  const uint8_t  INA_DEFAULT_OPERATING_MODE     =    B111; ///< Default continuous mode
+  const uint8_t  INA220_SHUNT_VOLTAGE_REGISTER  =       1; ///< INA220 Shunt Voltage Register
+  const uint8_t  INA220_CURRENT_REGISTER        =       4; ///< INA220 Current Register
+  const uint16_t INA220_BUS_VOLTAGE_LSB         =     400; ///< INA220 LSB in uV *100 4.00mV
+  const uint16_t INA220_SHUNT_VOLTAGE_LSB       =     100; ///< INA220 LSB in uV *10  10.0uV
+  const uint16_t INA220_CONFIG_ADCMODE_MASK     =  0x07F8; ///< INA220 Bits 3-6, 7-10
+  const uint16_t INA220_CONFIG_PG_MASK          =  0xE7FF; ///< INA220 Bits 11-12 masked
+  const uint16_t INA220_CONFIG_BADC_MASK        =  0x0780; ///< INA220 Bits 7-10  masked
+  const uint16_t INA220_CONFIG_SADC_MASK        =  0x0038; ///< INA220 Bits 3-5
+  const uint8_t  INA220_BRNG_BIT                =      13; ///< INA220 Bit for BRNG in config register
+  const uint8_t  INA220_PG_FIRST_BIT            =      11; ///< INA220 first bit of Programmable Gain
+  const uint8_t  INA220_BADC_FIRST_BIT          =       7; ///< INA220 first bit of Bus ADC configuration
+  const uint8_t  INA220_SADC_FIRST_BIT          =       3; ///< INA220 first bit of Shunt ADC configuration
+  const uint8_t  I2C_DELAY                      =      10; ///< Microsecond delay on I2C writes
+  const uint16_t INA220_CONFIG_DEFAULT          =  0x399F; ///< Default register value after reset
+
+  class INA220 {
+    public:
+      INA220                                ();
+      uint8_t     begin                     (uint8_t maxBusAmps, uint32_t microOhmR, const ina_Adc_Mode busAdcMode, const ina_Adc_Mode shuntAdcMode, const ina_Mode deviceMode, uint8_t* deviceAddresses, uint8_t numDevices);
+      void        setI2CSpeed               (const uint32_t i2cSpeed = INA_I2C_STANDARD_MODE);
+      void        setMode                   (const uint8_t  mode,     const uint8_t deviceNumber);
+      void        setModeAll                (const uint8_t  mode);
+      uint16_t    getBusMilliVolts          (const uint8_t  deviceNumber);
+      uint16_t    getBusRaw                 (const uint8_t  deviceNumber);
+      int32_t     getShuntMicroVolts        (const uint8_t  deviceNumber);
+      int16_t     getShuntRaw               (const uint8_t  deviceNumber);
+      int32_t     getBusMicroAmps           (const uint8_t  deviceNumber);
+      int32_t     getBusMicroWatts          (const uint8_t  deviceNumber);
+      void        triggerConversion         (const uint8_t  deviceNumber);
+      void        triggerConversionAll      ();
+      uint16_t    getMode                   (const uint8_t  deviceNumber);
+      uint8_t     getDeviceAddress          (const uint8_t  deviceNumber);
+      uint8_t     reset                     (const uint8_t  deviceNumber);
+      uint8_t     resetAll                  ();
+      bool        conversionFinished        (const uint8_t  deviceNumber);
+      bool        waitForConversion         (const uint16_t timeout, const uint8_t  deviceNumber);
+      uint8_t     waitForConversionAll      (const uint16_t timeout);
+    private:
+      void        initDevice                (const uint8_t  deviceNumber);
+      int16_t     readWord                  (const uint8_t  addr, const uint8_t  deviceAddress);
+      void        writeWord                 (const uint8_t  addr, const uint16_t data, const uint8_t deviceAddress);
+      uint8_t*    deviceAddresses;
+      uint8_t     numDevices;
+      uint32_t    current_LSB;
+      uint32_t    power_LSB;
+      uint16_t    calibrationRegister;
+      uint16_t    configRegister;
+  };
+#endif
